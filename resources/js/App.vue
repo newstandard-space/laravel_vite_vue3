98e1ad3e16f720b7099e638ed7387b74fb34d332
go-to-space <newstandardrise@gmail.com>
2023-03-01 11:14:13 +0900

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Counter from "./components/Counter.vue";
+import logo from "../images/logo-with-shadow.png";
+</script>
+
+<template>
+    <h1>Hello World</h1>
+    <img :src="logo" />
+    <Counter />
+</template>
---
+++ resources/js/components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const addCount = () => {
+    count.value = count.value + 1;
+};
+</script>
+
+<template>
+    <h2>Counter</h2>
+    <p>Count:{{ count }}</p>
+    <button @click="addCount">Add</button>
+</template>
